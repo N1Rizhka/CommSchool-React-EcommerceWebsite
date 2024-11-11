cf267078a0c08abde37738c876822457c29cdcc6
N1Rizhka <nrizh19@freeuni.edu.ge>
2024-11-12 01:13:18 +0400

    .

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -111,7 +111,7 @@ function initializeSearch() {
 // Call the appropriate function based on the page
 document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
-  if (path.includes("/Pages/catalogue.html")) { // Updated path to match the folder structure
+  if (path.includes("/Pages/catalogue.html")) {
     displayAllProducts(); // Call on the catalogue page
     initializeSearch();   // Initialize search on the catalogue page
   } else {
